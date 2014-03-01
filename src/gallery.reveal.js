55f5a8df4fb12f9b7a6ab462dad83cbfcfa5e3a9
Marcin Szczepanski <marcins@webqem.com>
2014-03-01 15:49:10 +1100

    Initial refactor
    Separate the Gallery code from the Reveal plugin to remove
    dependency on Reveal.  Add a Makefile to build the plugin script from
    the component parts.

---
+++ src/gallery.reveal.js
@@ -0,0 +1,16 @@
+(function() {
+	if( typeof window.addEventListener === 'function' ) {
+		Reveal.addEventListener("slidechanged", function (event) {
+			if (event.previousSlide.querySelector('.gallery')) {
+				Gallery.stop();
+			}
+
+			Gallery.start(event.currentSlide);
+		});
+
+		// during initial load
+		if (Reveal.getCurrentSlide()) {
+			Gallery.start(Reveal.getCurrentSlide());
+		}
+	}
+})();
